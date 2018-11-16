728d0aacc593c31f9ac1aad402cd21187f48944a
Daniel Izarra <36075925+DanielIzarra@users.noreply.github.com>
2018-11-16 17:34:50 +0000

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit()
+    {
+        $user = User::where('id', Auth::user()->id)
+                ->first();
+                
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
+
+        $user->name = request('name');
+
+        if (request('password')) {
+            $this->validate(request(), [
+                'password' => 'min:6|confirmed'
+            ]);
+
+            $user->password = bcrypt(request('password'));
+        }
+
+        if($validator->fails()) {
+            return Redirect::back()->withErrors($validator);
+        }
+        
+        $user->save();
+
+        return back()->with('status', 'Profile updated!');
+    } 
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
